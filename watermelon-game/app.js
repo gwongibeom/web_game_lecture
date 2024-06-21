a0c3b30cf66f739081a50a9fa38b1745858eb757
gwongibeom <k201gun@kakao.com>
2024-06-21 10:08:22 +0900

    (개발) 기능추가
    * 생성 예정 포지션 보기
    * 마우스 클릭시 생성
    * 같은 과일이 닿을시 합성

--- watermelon-game/app.js
+++ watermelon-game/app.js
@@ -1,3 +1,6 @@
+import { FRUITS } from './fruits.js'
+import { checkX } from './util/checkX.js'
+import { extractNumbers } from './util/extractNumbers.js'
 const Engine = Matter.Engine,
   Render = Matter.Render,
   Runner = Matter.Runner,
@@ -23,6 +26,8 @@ const render = Render.create({
 const world = engine.world
 
 const objects = {
+  //플랫폼
+
   leftWall: Bodies.rectangle(15, 395, 30, 790, {
     isStatic: true,
     render: { fillStyle: '#E6B143' },
@@ -37,9 +42,13 @@ const objects = {
   }),
   overLine: Bodies.rectangle(310, 150, 620, 2, {
     isStatic: true,
+    collisionFilter: false,
     render: { fillStyle: '#ff2400' },
   }),
-  circle: Bodies.circle(300, 200, 12, { isSleeping: true }),
+
+  // //오브젝트
+
+  // circle: Bodies.circle(300, 130, 12, { isSleeping: true }),
 }
 
 // 벽 배치
@@ -47,3 +56,85 @@ World.add(world, Object.values(objects))
 
 Render.run(render)
 Runner.run(engine)
+
+const canvas = document.querySelector('canvas')
+let index = 0
+let TempUserMouseLocationCircle = null
+
+canvas.addEventListener('mousemove', (e) => onMouseMove(e))
+const onMouseMove = (e) => {
+  if (TempUserMouseLocationCircle != null) {
+    Matter.Composite.remove(world, TempUserMouseLocationCircle)
+  }
+  const x = checkX(e.pageX)
+
+  const fruit = FRUITS[index]
+
+  const userMouseLocationCircle = Bodies.circle(x, 100, fruit.radius, {
+    collisionFilter: false,
+    isSleeping: true,
+    render: {
+      sprite: { texture: `${fruit.name}.png` },
+    },
+    restitution: 0.2,
+  })
+
+  TempUserMouseLocationCircle = userMouseLocationCircle
+
+  World.add(world, userMouseLocationCircle)
+}
+
+canvas.addEventListener('click', (e) => addFruit(e))
+const addFruit = (e) => {
+  // 과일 INDEX 저장
+
+  const fruit = FRUITS[index]
+
+  let x = checkX(e.pageX)
+
+  const body = Bodies.circle(x, 100, fruit.radius, {
+    render: {
+      sprite: { texture: `${fruit.name}.png` },
+    },
+    restitution: 0,
+  })
+
+  World.add(world, body)
+  index = Math.floor(Math.random() * 5)
+}
+
+Matter.Events.on(engine, 'collisionStart', (e) => {
+  e.pairs.map((collisionEvent) => {
+    const bodyATexture = collisionEvent.bodyA.render.sprite.texture
+    const bodyBTexture = collisionEvent.bodyB.render.sprite.texture
+
+    if (bodyATexture != bodyBTexture) return
+
+    console.log(collisionEvent)
+
+    const bodyAPosition = collisionEvent.bodyA.position
+    const bodyASpriteNum = extractNumbers(bodyATexture)
+    const bodyBSpriteNum = extractNumbers(bodyBTexture)
+
+    if (bodyASpriteNum === bodyBSpriteNum) {
+      Matter.Composite.remove(world, collisionEvent.bodyA)
+      Matter.Composite.remove(world, collisionEvent.bodyB)
+
+      const BIGGERFRUITS = FRUITS[Number(bodyASpriteNum) + 1]
+      console.log(bodyAPosition)
+
+      const BiggerCircle = Bodies.circle(
+        bodyAPosition.x,
+        bodyAPosition.y,
+        BIGGERFRUITS.radius,
+        {
+          render: {
+            sprite: { texture: `${BIGGERFRUITS.name}.png` },
+          },
+          restitution: 0.1,
+        }
+      )
+      World.add(world, BiggerCircle)
+    }
+  })
+})
---
+++ watermelon-game/fruits.js
@@ -0,0 +1,48 @@
+const FRUITS = [
+  {
+    name: '/watermelon-game/image/00_cherry',
+    radius: 33 / 2,
+  },
+  // name: "/suika/img/00_cherry", 다른 곳에서 작업할 경우 경로 확인
+  {
+    name: '/watermelon-game/image/01_strawberry',
+    radius: 48 / 2,
+  },
+  {
+    name: '/watermelon-game/image/02_grape',
+    radius: 61 / 2,
+  },
+  {
+    name: '/watermelon-game/image/03_gyool',
+    radius: 69 / 2,
+  },
+  {
+    name: '/watermelon-game/image/04_orange',
+    radius: 89 / 2,
+  },
+  {
+    name: '/watermelon-game/image/05_apple',
+    radius: 114 / 2,
+  },
+  {
+    name: '/watermelon-game/image/06_pear',
+    radius: 129 / 2,
+  },
+  {
+    name: '/watermelon-game/image/07_peach',
+    radius: 156 / 2,
+  },
+  {
+    name: '/watermelon-game/image/08_pineapple',
+    radius: 177 / 2,
+  },
+  {
+    name: '/watermelon-game/image/09_melon',
+    radius: 220 / 2,
+  },
+  {
+    name: '/watermelon-game/image/10_/watermelon',
+    radius: 259 / 2,
+  },
+]
+export { FRUITS }
